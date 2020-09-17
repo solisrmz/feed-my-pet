55334e12f65a005e51ef7d2418eacc82d38e6ee0
Solis <imsolis@hotmail.com>
2020-09-17 10:32:57 -0500

    Creación de clase para SharedPreference

---
+++ app/src/main/java/com/example/petfeeder/Shared/SharedPreference.kt
@@ -0,0 +1,23 @@
+package com.example.petfeeder.Shared
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPreference(val context: Context) {
+    private val PREFS_NAME = "mcu"
+    val sharedPref: SharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    fun save(KEY_NAME: String, text: String) {
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+        editor.putString(KEY_NAME, text)
+        editor!!.commit()
+    }
+    fun getValueString(KEY_NAME: String): Boolean? {
+        if (sharedPref.getString(KEY_NAME, null)!=null){
+            return true
+        }else{
+            return false
+        }
+    }
+}
+
--- app/src/main/java/com/example/petfeeder/Views/Splash.kt
+++ app/src/main/java/com/example/petfeeder/Views/Splash.kt
@@ -6,15 +6,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import com.example.petfeeder.R
+import com.example.petfeeder.Register
+import com.example.petfeeder.Shared.SharedPreference
 
 class Splash : AppCompatActivity() {
     private val SPLASH_TIME_OUT:Long = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
+        val sharedPreference: SharedPreference =SharedPreference(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash)
         Handler().postDelayed({
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
+                if (sharedPreference.getValueString("servo")!=false){
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }else{
+                    startActivity(Intent(this, Register::class.java))
+                    finish()
+                }
         }, SPLASH_TIME_OUT)
     }
 }
